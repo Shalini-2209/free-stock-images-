5ea79cd938b323e7a0d0a598ad925497afdde44c
Anbarsan <arsan.anbu97@gmail.com>
2021-01-13 12:28:27 +0530

    Revised Code

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask, flash, render_template,request,redirect,url_for
 import pymongo
 from pymongo import MongoClient
-import os
-import json
+from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 from bson.objectid import ObjectId
+import os
+import json
 
-app = Flask(__name__)
 load_dotenv()
 
 URI = os.getenv("MONGO_URI")
 
-
 cluster = MongoClient(URI)
-db = cluster.drive
-img_collection = db.images
+db = cluster["imagesDb"]
+collection = db["images"]
+
+UPLOAD_FOLDER = './static/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def home():
-    documents = img_collection.find()
+    documents = collection.find()
+    print(documents)
     return render_template('index.html', documents = documents)
 
 @app.route('/upload')
@@ -31,24 +37,30 @@ def upload():
 def insert():
   
     if request.method == 'POST':
-        file = request.form['img']
         name = request.form['owner'].lower()
-
-        post = {'img_src': file, 'owner':name}
-       
-        img_collection.insert_one(post)
-        return  redirect('/')
+        file = request.files['file']
         
-    return redirect('/upload')   
+        if file.filename == '':
+            return redirect('/')
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            collection.insert_one({'owner':name,"filename": filename})
+
+    return redirect('/')
 
 
 @app.route('/delete/<id>')
 def delete(id):
-    try:
-        img_collection.delete_one({"_id":ObjectId(id)})
-        return redirect('/')   
-    except:
-        redirect('/upload')   
+    document = collection.find_one({"_id":ObjectId(id)})
+    filename = document["filename"]
+
+    if os.path.exists(os.path.join( app.config['UPLOAD_FOLDER'] , str (filename))) :
+        os.unlink(os.path.join( app.config['UPLOAD_FOLDER'] , str (filename)))
+    collection.delete_one({"_id":ObjectId(id)})
+
+    return redirect('/')   
 
 
 @app.route('/search', methods=['GET', 'POST'])
@@ -56,12 +68,16 @@ def search():
     try:
         photographer = request.form['search']
         photographer = photographer.lower()
-        documents = img_collection.find({'owner': photographer})
-        print(documents)
+        documents = collection.find({'owner': photographer})
         return render_template('index.html', documents = documents)
-
     except:
         print('Invalid Data')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 if(__name__ == "__main__"):
     app.run(debug=True)
